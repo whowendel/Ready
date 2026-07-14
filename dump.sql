113869b2ef88626c41216b5eb4acfe9f62bb8703
whowendel <wendellacson.1@gmail.com>
2026-07-14 20:28:20 +0800

    env

---
+++ dump.sql
@@ -0,0 +1,43 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict SwkKZdaBEvG0cDw6HhUOVRqxynnpfZwf0sJO8mWdZbJlGgIbBe9j7f3BJlLc9UL
+
+-- Dumped from database version 16.14
+-- Dumped by pg_dump version 16.14
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP SCHEMA IF EXISTS public;
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
+--
+
+CREATE SCHEMA public;
+
+
+ALTER SCHEMA public OWNER TO pg_database_owner;
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict SwkKZdaBEvG0cDw6HhUOVRqxynnpfZwf0sJO8mWdZbJlGgIbBe9j7f3BJlLc9UL
+
